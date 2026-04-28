49f5f4735e072763c3f49a1f09a86328cb7f1591
AiresEllen <ellen.costa.aires@gmail.com>
2026-04-28 18:04:19 -0300

    move firebase config to env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCj7F799Gcc_KXys-Rqd-ij6Kzw7JlLhfs",
-  authDomain: "topdrinks-200d7.firebaseapp.com",
-  projectId: "topdrinks-200d7",
-  storageBucket: "topdrinks-200d7.firebasestorage.app",
-  messagingSenderId: "827903755756",
-  appId: "1:827903755756:web:6fc2ffe43d10b1419d2cc9",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
